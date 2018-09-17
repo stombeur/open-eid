23c88b43c9452b9bfa82f6963314b63ef41aa4f7
Michael Bxl <michael@barchy.be>
2018-09-17 22:33:25 +0200

    Fix + Debug

--- src/chrome/contentScript.js
+++ src/chrome/contentScript.js
@@ -25,20 +25,22 @@ window.addEventListener('message', function(event) {
   if(event.source != window) return;
 
   // send request to extension
-  if('url' in event.data) {
-    event.data.url = event.data.url + new String(event.source.location);
-    if(typeof browser != 'undefined' && browser != null) {
-      console.log('Window: Firefox');
-      browser.runtime.sendMessage(event.data);        
+  if(typeof event.data == 'object') {
+    if('url' in event.data) {
+      event.data.url = event.data.url + new String(event.source.location);
+      if(typeof browser != 'undefined' && browser != null) {
+        console.log('Window: Firefox');
+        browser.runtime.sendMessage(event.data);        
+      } else {
+        console.log('Window: Chrome');
+        chrome.extension.sendMessage(event.data);
+      }
     } else {
-      console.log('Window: Chrome');
-      chrome.extension.sendMessage(event.data);
+      var data = event.data;
+      for(var i in data) data[i] = decodeURIComponent(data[i]);
+      console.log(data);
+      window.localStorage.setItem('open-eid', JSON.stringify(data));
     }
-  } else {
-    var data = event.data;
-    for(var i in data) data[i] = decodeURIComponent(data[i]);
-    console.log(data);
-    window.localStorage.setItem('open-eid', JSON.stringify(data));
   }
   
 }, false);
