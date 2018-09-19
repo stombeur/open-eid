457c524029264f9855c39f029d72344ff09f74a8
Michael Bxl <michael@barchy.be>
2018-09-19 18:13:28 +0200

    Windows Make

--- src/make.cmd
+++ src/make.cmd
@@ -1,10 +1,17 @@
 @echo off
+
 cd win
 ..\..\tools\bin\win32\gorc open-eid.rc
 ..\..\tools\fbc -s gui open-eid.bas open-eid.res
 del open-eid.res
-
-copy /b open-eid.exe+Swelio32.sign+..\..\tools\Swelio32.dll ..\..\release\Open-eID.exe
+del ..\..\build\Open-eID.exe
+copy /b open-eid.exe+Swelio32.sign+..\..\tools\Swelio32.dll ..\..\build\Open-eID.exe
 
 del open-eid.exe 
+del ..\..\release\Open-eID.zip
+cd ..
+cd ..
+cd tools
+7za a ..\release\Open-eID.zip ..\build\Open-eID.exe
+
 pause
