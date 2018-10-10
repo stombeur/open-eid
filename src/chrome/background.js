6295abe2d7652cce90076782e566f493ba8ceff7
Michael Bxl <michael@barchy.be>
2018-10-10 19:18:10 +0200

    Extension security model + Sign test

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -21,3 +21,99 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     }
   );   
 });
+
+chrome.permissions.getAll(function(permissions) {
+  console.log(permissions.origins);
+  var origins = permissions.origins;
+  origins.push('file:');
+  for(var i in origins) {
+    var origin = origins[i];
+    chrome.webNavigation.onCompleted.addListener(function(details) {
+      console.log(details);
+      try {
+        chrome.tabs.executeScript(details.tabId, {
+          file: "contentScript.js",
+          runAt: "document_end"
+        }, function(result) {
+          var lastErr = chrome.runtime.lastError;
+          if(lastErr) console.log(lastErr);
+        });  
+      } catch(e) {
+        // not allowed
+      }    
+    }, {url: [{urlPrefix: origin.substring(0, origin.length - 2)}]});
+  }
+});
+
+//chrome.webNavigation.onCompleted.addListener
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log('Location: ' + tab.url);
+  var parts = [];
+  parts = tab.url.split(':');
+  var protocol = parts[0];
+  if(parts.length > 0) parts = parts[1].split('\/');
+  var host = '';
+  for(var i in parts) {
+    if(parts[i] != '') {
+      host = parts[i];
+      break;
+    }
+  }
+  console.log('Origin: ' + protocol + '://' + host + '/*');
+  if(host == '') return;
+  var origin = protocol + '://' + host + '/*';
+  var permission = {
+    permissions: ['tabs', 'webNavigation'],
+    origins: [protocol + '://' + host + '/*']
+  };
+  chrome.permissions.contains(permission, function(result) {
+    if(result) {
+      console.log('Extension has access to ' + origin);
+      chrome.browserAction.setBadgeBackgroundColor({color: '#79d3af'});
+      chrome.browserAction.setBadgeText({text: '\u2714'});
+      if(confirm('Open e-ID has access to ' + host + '\n\nClick Cancel to remove the authorization')) {
+        // ok
+      } else {
+        console.log('Permission remove request for ' + origin);
+        chrome.permissions.remove(permission, function(removed) {
+          if(removed) {
+            console.log('Permission removed for ' + origin);
+            chrome.browserAction.setBadgeBackgroundColor({color: 'red'});
+            chrome.browserAction.setBadgeText({text: '\u2715'});
+            alert('The permission was removed');
+            setTimeout(function() {
+              chrome.browserAction.setBadgeText({text: ''});
+            }, 2000);            
+          } else {
+            alert('Open e-ID has access to ' + host);
+          }
+        }); 
+        setTimeout(function() {
+          chrome.browserAction.setBadgeText({text: ''});
+        }, 2000);               
+      }
+      setTimeout(function() {
+        chrome.browserAction.setBadgeText({text: ''});
+      }, 2000);      
+    } else {
+      console.log('Permission request for ' + origin);
+      chrome.browserAction.setBadgeBackgroundColor({color: 'orange'});
+      chrome.browserAction.setBadgeText({text: '?'});
+      chrome.permissions.request(permission, function(granted) {
+        if(granted) {
+          console.log('Extension has access to ' + origin);
+          chrome.browserAction.setBadgeBackgroundColor({color: '#79d3af'});
+          chrome.browserAction.setBadgeText({text: '\u2714'});
+        } else {
+          console.log('Permission NOT granted - extension has NO access to ' + origin);
+          chrome.browserAction.setBadgeBackgroundColor({color: 'red'});
+          chrome.browserAction.setBadgeText({text: '\u2715'});
+        }
+        setTimeout(function() {
+          chrome.browserAction.setBadgeText({text: ''});
+        }, 2000);        
+      });      
+    }
+  });  
+});
--- src/mac/open-eid.js
+++ src/mac/open-eid.js
@@ -191,14 +191,15 @@ function eid(confirm) {
          native(obj);
        }
      }
-     if(args[0].indexOf('open-eid-sign:') == 0) {
+     if(args[0].indexOf('open-eid-sign:') == 0) {       
        fs.writeFileSync(homedir + '/data', 'Hello');       
        pkcs11.C_DigestInit(session, { mechanism: pkcs11js.CKM_SHA256 });
        pkcs11.C_DigestUpdate(session, new Buffer("Hello"));
        var digest = pkcs11.C_DigestFinal(session, Buffer(256 / 8));
+       console.log(digest.toString('base64'));
        var header = 'Manifest-Version: 1.0\r\nCreated-By: 11 (Oracle Corporation)\r\n\r\n';
        var main = 'Name: data\r\nSHA-256-Digest: ' + digest.toString("base64") + '\r\n\r\n';
-       fs.writeFileSync(homedir + '/MANIFEST.MF', header + main);       
+       fs.writeFileSync(homedir + '/META-INF/MANIFEST.MF', header + main);       
        pkcs11.C_DigestInit(session, { mechanism: pkcs11js.CKM_SHA256 });
        pkcs11.C_DigestUpdate(session, new Buffer(header + main));
        var digest = pkcs11.C_DigestFinal(session, Buffer(256 / 8));
@@ -212,7 +213,7 @@ function eid(confirm) {
        var digest = pkcs11.C_DigestFinal(session, Buffer(256 / 8));
        var main = digest.toString("base64")
        var secpad = 'Signature-Version: 1.0\r\nCreated-By: 11 (Oracle Corporation)\r\nSHA-256-Digest-Manifest: ' + manifest + '\r\nSHA-256-Digest-Manifest-Main-Attributes: ' + header + '\r\n\r\nName: data\r\nSHA-256-Digest: ' + main + '\r\n\r\n';        
-       fs.writeFileSync(homedir + '/SEC_PAD.SF', secpad);       
+       fs.writeFileSync(homedir + '/META-INF/SEC_PAD.SF', secpad);       
        pkcs11.C_FindObjectsInit(session, [{ type: pkcs11js.CKA_CLASS, value: pkcs11js.CKO_PRIVATE_KEY }]);
         var hObject = pkcs11.C_FindObjects(session);
         while (hObject) {
@@ -224,10 +225,18 @@ function eid(confirm) {
             if (attrs[1].value[0]){
                 console.log(`Object #${hObject}: ${attrs[2].value.toString()}`);
                 if(attrs[2].value.toString() == 'Signature') { // CKM_SHA256_RSA_PKCS
-                  pkcs11.C_SignInit(session, { mechanism: pkcs11js.CKM_SHA1_RSA_PKCS }, hObject); // hObject = privateKey
-                  pkcs11.C_SignUpdate(session, new Buffer(secpad));
-                  var signature = pkcs11.C_SignFinal(session, Buffer(256));
-                  console.log(signature.toString('base64'));
+                  pkcs11.C_SignInit(session, { mechanism: pkcs11js.CKM_RSA_PKCS }, hObject); // hObject = privateKey
+                  //pkcs11.C_SignUpdate(session, new Buffer(secpad));
+                  //var signature = pkcs11.C_SignFinal(session, Buffer(256));
+                  var hashWithPadding = [0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20];
+                  var signWithPadding = [0x30, 0x82, 0x01, 0xa6, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x07, 0x02, 0xA0, 0x82, 0x01, 0x97, 0x30, 0x82, 0x01, 0x93, 0x02, 0x01, 0x01, 0x31, 0x0F, 0x30, 0x0D, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x30, 0x0B, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x07, 0x01, 0x31, 0x82, 0x01, 0x6E, 0x30, 0x82, 0x01, 0x6A, 0x02, 0x01, 0x01, 0x30, 0x43, 0x30, 0x36, 0x31, 0x34, 0x30, 0x32, 0x06, 0x03, 0x55, 0x04, 0x03, 0x0C, 0x2B, 0x41, 0x6E, 0x6F, 0x74, 0x68, 0x65, 0x72, 0x20, 0x63, 0x65, 0x72, 0x74, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x65, 0x20, 0x77, 0x69, 0x74, 0x68, 0x20, 0x61, 0x20, 0x6D, 0x75, 0x63, 0x68, 0x20, 0x6C, 0x6F, 0x6E, 0x67, 0x65, 0x72, 0x20, 0x6E, 0x61, 0x6D, 0x65, 0x02, 0x09, 0x00, 0xFC, 0x40, 0x46, 0x11, 0x5A, 0x53, 0x2A, 0x1E, 0x30, 0x0D, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x30, 0x0D, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x01, 0x01, 0x05, 0x00, 0x04, 0x82, 0x01, 0x00];                  
+                  var hashBinary = Buffer.concat([new Buffer(hashWithPadding), digest]);
+                  console.log(hashBinary.toString('base64')); 
+                  console.log('--');
+                  var signature = pkcs11.C_Sign(session, hashBinary, new Buffer(256));
+                  console.log(signature.length);
+                  console.log(Buffer.concat([new Buffer(signWithPadding), signature]).toString('base64'));
+                  fs.writeFileSync(homedir + '/SEC_PAD.PEM', Buffer.concat([new Buffer(signWithPadding), signature]).toString('base64')); 
                 }
             }
             hObject = pkcs11.C_FindObjects(session);
@@ -246,4 +255,5 @@ function eid(confirm) {
   }
 
 }
+
 
